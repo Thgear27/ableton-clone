d1b2157d6f1efd05ee87df8b1ccb3bcd289d3ce1
Fernando Vegas <vegasfernando2003@gmail.com>
2023-05-03 13:00:17 -0500

    Mobile menu bug fixed

--- src/index.js
+++ src/index.js
@@ -16,13 +16,11 @@ function main() {
             svgLogo.setAttribute("show-menu", "true");
             mobileMenu.setAttribute("show-menu", "true");
             menuList.setAttribute("show-menu", "true");
-            disableScroll();
         } else {
             svgAngle.setAttribute("show-menu", "false");
             svgLogo.setAttribute("show-menu", "false");
             mobileMenu.setAttribute("show-menu", "false");
             menuList.setAttribute("show-menu", "false");
-            enableScroll();
         }
     });
 }
